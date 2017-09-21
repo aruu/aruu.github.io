e661d302210e5f9a51fefe6dcbcaf66c04d2bea9
Kevin Thai <kvn.thai@gmail.com>
2017-09-21 00:26:02 -0400

    Better structuring of JavaScript loading

--- common.js
+++ common.js
@@ -1,18 +1,20 @@
-function fixRelativeLinks() {
-	if (document.URL.match("file://")) return;
+// scripts that should be executed for all pages on the site
+
+// Fix relative links
+if (!document.URL.match("file://")) {
 	var elements = document.getElementsByClassName("asset");
 
-  for (var i=0; i<elements.length; i++) {
-  	var attributes = elements[i].attributes;
+	for (var i=0; i<elements.length; i++) {
+		var attributes = elements[i].attributes;
+		for (var j=0; j<attributes.length; j++) {
+			var old = attributes[j].value;
 
-	  for (var j=0; j<attributes.length; j++) {
-  		var old = attributes[j].value;
+			if (old.match(/assets\//) && attributes[j].name !== "id") {
+				attributes[j].value = old.replace(/assets\//,
+																					"https://aruu-b21e7.firebaseapp.com/assets/");
+			}
 
-	  	if (old.match(/assets\//) && attributes[j].name !== "id") {
-	  		attributes[j].value = old.replace(/assets\//, "https://aruu-b21e7.firebaseapp.com/assets/");
-	  	}
+		}
 
-	  }
-
-  }
+	}
 }
