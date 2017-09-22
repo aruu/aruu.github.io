da9f663d6568fc125b0a0f313675bf0a629af6cb
Kevin Thai <kvn.thai@gmail.com>
2017-09-21 20:05:53 -0400

    Making the commit display more informative

--- commit-banner.js
+++ commit-banner.js
@@ -5,35 +5,45 @@ const url = 'https://api.github.com/users/aruu/events/public';
 fetch(url)
 .then((resp) => resp.json())
 .then(function(events) {
-	let i, event;
+	var i, event;
 	for (i=0; i<events.length; i++) {
 		if ("PushEvent" === events[i].type) {
 			event = events[i];
 			break;
 		}
 	}
-
-	let url2 = event.payload.commits[0].url;
-	fetch(url2)
-	.then((resp) => resp.json())
-	.then(function(commit) {
-	  let p = document.createElement("P"),
-	  		a = document.createElement("a"),
-	  		t = document.createTextNode("Most recent public commit: ");
-	  a.href = commit.html_url;
-	  a.innerHTML = commit.html_url;
-	  a.style.fontSize = "14px";
-	  p.appendChild(t);
-	  p.appendChild(a);
-	  commitBanner.appendChild(p);
-	  return commit.html_url;
-	})
-	.catch(function(error) {
-	  console.log(error);
-	});
-
-  return;
+  return event;
 })
-.catch(function(error) {
-  console.log(error);
-});
+.then(function(event) {
+	var repo_url = event.repo.url,
+	 		commmit_url = event.payload.commits[0].url;
+
+	fetch(repo_url)
+	.then((resp) => resp.json())
+	.then(function(repo) {
+
+		fetch(commmit_url)
+		.then((resp) => resp.json())
+		.then(function(commit) {
+		  let p = document.createElement("p"),
+		  		a1 = document.createElement("a"),
+		  		a2 = document.createElement("a"),
+		  		t1 = document.createTextNode("My most recent commit: ")
+		  		t2 = document.createTextNode(" in "),
+		  		strong = document.createElement("strong");
+
+		  strong.appendChild(t1);
+		  a1.href = commit.html_url;
+		  a1.innerHTML = event.payload.commits[0].message;
+		  a2.href = repo.html_url;
+		  a2.innerHTML = event.repo.name;
+		  p.appendChild(strong);
+		  p.appendChild(document.createElement("br"));
+		  p.appendChild(a1);
+		  p.appendChild(t2);
+		  p.appendChild(a2);
+		  commitBanner.appendChild(p);
+		  return;
+		})
+	})
+})
